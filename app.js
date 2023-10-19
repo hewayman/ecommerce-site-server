71ddc5acaff758d255044fb7d74d3c4e3ac64652
hewayman <hewayman@gmail.com>
2023-10-19 12:22:51 -0400

    Add encryption

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
+
 const database = require('./models/index');
-
-// database.sequelize.sync();
-
-database.sequelize.sync({ force: true });
-
 const user = require('./controllers/user_controller');
 
+database.sequelize.sync();
+
 app.use(express.json());
+// database.sequelize.sync({ force: true });
+
+// const site = require('./controllers/site_controller');
 
 app.use('/user', user);
+app.use(require('./middleware/validate-session'));
 
 app.listen(port, () => console.log(`App is listening on port ${port}`));
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,24 +1,13 @@
 const router = require('express').Router();
 const sequelize = require('../models');
-// const User = require('../db').import('../models/user');
 const User = sequelize.users;
 const jwt = require('jsonwebtoken');
-// const { database } = require('../db');
-// const User = require('../models/user');
-
-// const handleLoginSuccess = (user) => {
-//   console.log('here');
-//   if (user === null || user === 'undefined') {
-//     res.status(500).json({ error: 'User does not exist' });
-//   }
-
-//   res.status(200).json({ user });
-// };
+const bcrypt = require('bcryptjs');
 
 router.post('/register', (req, res) => {
   User.create({
     email: req.body.user.email,
-    password: req.body.user.password,
+    password: bcrypt.hashSync(req.body.user.password),
     isAdmin: req.body.user.isAdmin,
     firstName: req.body.user.firstName,
     lastName: req.body.user.lastName,
@@ -37,26 +26,36 @@ router.post('/register', (req, res) => {
     .catch((error) => res.status(500).json({ error }));
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   User.findOne({
     where: {
       email: req.body.user.email,
     },
   })
     .then((user) => {
-      if (user) {
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-          expiresIn: '1d',
-        });
-
-        res.status(200).json({
-          user,
-          message: 'User login successful',
-          sessionToken: token,
-        });
-      } else {
-        res.status(500).json({ error: 'User does not exist' });
+      if (!user) {
+        return res.status(500).json({ error: 'User does not exist' });
       }
+
+      bcrypt.compare(
+        req.body.user.password,
+        user.password,
+        (error, isMatch) => {
+          if (error || !isMatch) {
+            return res.status(502).json({ error: 'Login failed' });
+          }
+
+          const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+            expiresIn: '1d',
+          });
+
+          return res.status(200).json({
+            user,
+            message: 'User login successful',
+            sessionToken: token,
+          });
+        }
+      );
     })
     .catch((error) => res.status(500).json({ error }));
 });
